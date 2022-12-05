bcedd4423e9bd337f3433c267ba28f7fb312e112
MistEO <mistereo@hotmail.com>
2022-12-05 17:09:36 +0800

    feat: 初始化文本生成工具

---
+++ Vision/OCR/generate.py
@@ -0,0 +1,69 @@
+import re
+import os
+
+
+print('Please clone these third-party repositories manually into your local directory'
+      '- https://github.com/Kengxxiao/ArknightsGameData')
+
+
+def parse_line(line):
+    result = set()
+    in_string = False
+    string_start = 0
+    string_end = 0
+    pre_char = ""
+    has_non_ascii = False
+    for index in range(len(line)):
+        char = line[index]
+        if char == "\"" and pre_char != "\\":
+            if not in_string:
+                string_start = index + 1
+            else:
+                string_end = index
+                if has_non_ascii:
+                    wording = line[string_start:string_end]
+                    wording = re.sub(r"<.*?>", "", wording)
+                    wording = re.sub(r"{.*?}", "", wording)
+                    wording = wording.replace("\\\\", "\\")
+                    wording = wording.replace("\\n", "\n")
+                    wording = wording.replace("\\t", "\n")
+                    wording = wording.replace("\t", "\n")
+                    result.update(
+                        set([w for w in wording.split("\n") if w and w != ' ']))
+            in_string = not in_string
+            has_non_ascii = False
+        elif in_string and ord(char) > 127:
+            has_non_ascii = True
+        pre_char = char
+    return result
+
+
+def find_all_wording(dir):
+    result = set()
+    for root, _, files in os.walk(dir):
+        for file in files:
+            if not file.endswith(".json"):
+                continue
+            with open(os.path.join(root, file), "r", encoding="utf-8") as f:
+                for line in f.readlines():
+                    result.update(parse_line(line))
+    return result
+
+
+for root, dirs, _ in os.walk('ArknightsGameData'):
+    for client in dirs:
+        wording = find_all_wording(os.path.join(
+            root, client, 'gamedata', 'excel'))
+        if not wording:
+            continue
+        wording.update(set([chr(x) for x in range(33, 127)]))
+        output_dir = os.path.join('output', client)
+        os.makedirs(output_dir, exist_ok=True)
+        context = '\n'.join(wording)
+        with open(os.path.join(output_dir, 'wording.txt'), 'w', encoding='utf-8') as f:
+            f.write(context)
+        keys = set()
+        for k in context:
+            keys.add(k)
+        with open(os.path.join(output_dir, 'keys.txt'), 'w', encoding='utf-8') as f:
+            f.write('\n'.join(keys))
