5e0bfd8b5afe3b61b6a14f971f32ee4851550130
MistEO <mistereo@hotmail.com>
2023-03-18 22:17:45 +0800

    fix: 修正干员检测测试代码错误

--- combat/operators/test.py
+++ combat/operators/test.py
@@ -4,5 +4,4 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     # Load a model
     model = YOLO("runs/detect/train/weights/best.onnx")
-    model.val()
     results = model("datasets/test/test.mp4", save=True)  # predict on an image
